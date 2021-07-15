7427e89ec8a6386da6d91fbe29b8dd687f1374d0
Alexander Wilson <aw@zndr.co.uk>
2021-07-15 13:50:31 +0100

    rfid name

--- testingScripts/testReader.py
+++ testingScripts/testReader.py
@@ -1,6 +1,8 @@
-with RfidReader("Barcode Reader") as rfid:
+from rfidkeyboard import *
+
+with RfidReader("Sycreader RFID Technology Co., Ltd SYC ID&IC USB Reader") as rfid:
     while True:
         card = rfid.read()
         if card is not None:
             print("SCANNED: " + card)
-        time.sleep(0.050) # HACK: Don’t busy-wait the CPU too much
+        time.sleep(0.050) # HACK Dont busy-wait the CPU too much
