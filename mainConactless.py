952aefa4ad7a3e3f53b875d9a60739b1c4edff5f
zndrp <aw@zndr.co.uk>
2021-07-15 13:43:52 +0100

    not none

--- mainConactless.py
+++ mainConactless.py
@@ -270,7 +270,7 @@ def main():
 				tag_id = card
 			time.sleep(0.050)
 
-		if not tag_id = None
+		if tag_id is not None
 			print "contactless record protocol initiated"
 			print puz_file
 			record(puz_file)
